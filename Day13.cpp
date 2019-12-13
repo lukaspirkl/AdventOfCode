96191d5551dfd115e6486e19c72bc6eade6ea2a9
Lukas Pirkl <pirkl.lukas@gmail.com>
2019-12-13 20:04:12 +0100

    Day13

---
+++ Day13.cpp
@@ -0,0 +1,99 @@
+#include "pch.h"
+#include "aoc.h"
+#include "intcode.h"
+
+namespace
+{
+	int ai(std::vector<std::vector<int>>& screen)
+	{
+		size_t xball = 0;
+		size_t xpaddle = 0;
+
+		for (size_t x = 0; x < screen.size(); x++)
+		{
+			for (size_t y = 0; y < screen[x].size(); y++)
+			{
+				if (screen[x][y] == 3) { xpaddle = x; }
+				if (screen[x][y] == 4) { xball = x; }
+			}
+		}
+		if (xball == xpaddle) { return 0; }
+		else if (xball < xpaddle) { return -1; }
+		else { return 1; }
+	}
+
+	TEST(Day13, InputA)
+	{
+		aoc::intcode intcode(aoc::readInputFile("Day13.txt").str());
+		std::map<aoc::Coords, int> screen;
+
+		aoc::intcode::result result;
+		while (true)
+		{
+			aoc::Coords c;
+			
+			result = intcode.run();
+			if (result == aoc::intcode::result::programEnded) { break; }
+			c.x = (int)intcode.io;
+			
+			result = intcode.run();
+			if (result == aoc::intcode::result::programEnded) { break; }
+			c.y = (int)intcode.io;
+
+			result = intcode.run();
+			if (result == aoc::intcode::result::programEnded) { break; }
+			screen.insert({ c, (int)intcode.io });
+		}
+
+		int blockCount = 0;
+		for (auto& pair : screen)
+		{
+			if (pair.second == 2) { blockCount++; }
+		}
+
+		EXPECT_EQ(blockCount, 315);
+	}
+
+	TEST(Day13, InputB)
+	{
+		auto input = aoc::splitLongLong(aoc::readInputFile("Day13.txt").str());
+		input[0] = 2;
+		aoc::intcode intcode(input);
+		std::vector<std::vector<int>> screen(45, std::vector<int>(45, 0));
+		int score = 0;
+
+		aoc::intcode::result result;
+		int readCount = 0;
+		int out[3];
+		while (true)
+		{
+			result = intcode.run();
+			if (result == aoc::intcode::result::programEnded)
+			{
+				break;
+			}
+			if (result == aoc::intcode::result::outputProvided)
+			{
+				out[readCount] = (int)intcode.io;
+				readCount++;
+				if (readCount == 3)
+				{
+					readCount = 0;
+					if (out[0] == -1 && out[1] == 0)
+					{
+						score = out[2];
+					}
+					else
+					{
+						screen[out[0]][out[1]] = out[2];
+					}
+				}
+			}
+			if (result == aoc::intcode::result::inputRequested)
+			{
+				intcode.io = ai(screen);
+			}
+		}		
+		EXPECT_EQ(score, 16171);
+	}
+}
