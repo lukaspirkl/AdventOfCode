3a96950b855b13ba8227f178ceec35b9ef17c193
Lukas Pirkl <pirkl.lukas@gmail.com>
2019-12-29 12:36:06 +0100

    2019 Day16 - Revert optimization attempt

--- 2019/Solutions/Day16.cpp
+++ 2019/Solutions/Day16.cpp
@@ -16,33 +16,6 @@ namespace
 		return nums;
 	}
 
-	// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
-	// 1  0 -1  0  1  0 -1  0  1  0 -1  0  1  0 -1  
-	// + = 0  4  8 12 ->  +0(+4)
-	// - = 2  6 10 14 ->  +2(+4)
-
-	// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22
-	// 0  1  1  0  0 -1 -1  0  0  1  1  0  0 -1 -1  0  0  1  1  0  0 -1 -1
-	// + = 1  2  9 10 17 18 ->  +1(+1+7)
-	// - = 5  6 13 14 21 22 ->  +5(+1+7)
-
-	// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
-	// 0  0  1  1  1  0  0  0 -1 -1 -1  0  0  0  1  1  1
-	// + = 2  3  4 14 15 16 ->  +2(+1+1+10)
-	// - = 8  9 10 20 21 22 ->  +8(+1+1+10)
-
-	// 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22
-	// 0  0  0  1  1  1  1  0  0  0  0 -1 -1 -1 -1  0  0  0  0  1  1  1  1
-	// ->  +3(+1+1+1+13)
-	// -> +11(+1+1+1+13)
-
-	// ->  +4(+1+1+1+1+16)
-	// -> +14(+1+1+1+1+16)
-
-	// x = 4
-	// +[i] ( (+[1])i +[4+i*] )untilend
-	
-
 
 	std::string calculate(std::vector<int> nums, int phases, int offset = 0)
 	{
@@ -54,58 +27,7 @@ namespace
 			std::vector<int> newNums;
 			for (size_t i = 0; i < nums.size(); i++)
 			{	
-				size_t idx = i;
-				int sum = nums[idx];
-				if (i == 0)
-				{
-					if ((idx + x - 2) < nums.size())
-					{
-						sum -= nums[idx + x - 2];
-					}
-					while (idx < nums.size())
-					{
-						idx += x;
-						if (idx < nums.size())
-						{
-							sum += nums[idx];
-						}
-						if ((idx + x - 2) < nums.size())
-						{
-							sum -= nums[idx + x - 2];
-						}
-					}
-				}
-				else
-				{
-					if ((idx + x - 3) < nums.size())
-					{
-						sum -= nums[idx + x - 3];
-					}
-					while (idx < nums.size())
-					{
-						for (size_t j = 0; j < i && idx + 1 < nums.size(); j++)
-						{
-							idx += 1;
-							sum += nums[idx];
-							if ((idx + x - 3) < nums.size())
-							{
-								sum -= nums[idx + x - 3];
-							}
-						}
-						idx += x;
-						if (idx < nums.size())
-						{
-							sum += nums[idx];
-						}
-						if ((idx + x - 3) < nums.size())
-						{
-							sum -= nums[idx + x - 3];
-						}
-					}
-				}
-				x += 3;
-				/*
-				int sum2 = 0;
+				int sum = 0;
 				std::vector<int> p;
 				for (size_t j = 0; true; j++)
 				{
@@ -119,9 +41,9 @@ namespace
 
 				for (size_t j = 0; j < nums.size(); j++)
 				{
-					sum2 += nums[j] * p[j + 1];
+					sum += nums[j] * p[j + 1];
 				}
-				*/
+
 				newNums.push_back(std::abs(sum) % 10);
 			}
 			nums = newNums;
@@ -179,28 +101,32 @@ namespace
 	TEST(NAME, ExampleB2)
 	{
 		FAIL() << "Slow as hell and maybe wrong";
-		auto nums = getMegaNums(create("03036732577212944063491565474664"));
+		auto n = create("03036732577212944063491565474664");
+		auto nums = getMegaNums(n);
 		EXPECT_EQ(calculate(nums, 100, 303673), "84462026");
 	}
 
 	TEST(NAME, ExampleB3)
 	{
 		FAIL() << "Slow as hell and maybe wrong";
-		auto nums = getMegaNums(create("02935109699940807407585447034323"));
+		auto n = create("02935109699940807407585447034323");
+		auto nums = getMegaNums(n);
 		EXPECT_EQ(calculate(nums, 100, 293510), "78725270");
 	}
 
 	TEST(NAME, ExampleB4)
 	{
 		FAIL() << "Slow as hell and maybe wrong";
-		auto nums = getMegaNums(create("03081770884921959731165446850517"));
+		auto n = create("03081770884921959731165446850517");
+		auto nums = getMegaNums(n);
 		EXPECT_EQ(calculate(nums, 100, 308177), "53553731");
 	}
 
 	TEST(NAME, InputB)
 	{
 		FAIL() << "Slow as hell and maybe wrong";
-		auto nums = getMegaNums(create(aoc::readInputFile("Day16.txt").str()));
+		auto n = create(aoc::readInputFile("Day16.txt").str());
+		auto nums = getMegaNums(n);
 		EXPECT_EQ(calculate(nums, 100, 5970221), "");
 	}
 }
