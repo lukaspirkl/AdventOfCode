b300ffec90f0a1f9abf7ed9ab822577ad09f274d
Lukas Pirkl <pirkl.lukas@gmail.com>
2020-01-03 18:55:39 +0100

    2019 Day24

--- 2019/Solutions/Day24.cpp
+++ 2019/Solutions/Day24.cpp
@@ -151,35 +151,247 @@ namespace
 		}
 	}
 
-	typedef std::map<int, map> multimap;
+	typedef std::vector<map> multimap;
 
-	multimap multiCreate(const std::string& input)
+	char getAdjacentBugs(const multimap& m, const size_t& layer, const size_t& x, const size_t& y)
 	{
-		return multimap{ { 0, create(input) } };
-	}
-
-	/*
-	bool get(const multimap& m, const int& layer, const size_t& x, const size_t& y)
-	{
-		if (x < 0 || y < 0 || y >= 5 || x >= 5)
+		if (y == 2 && x == 2)
 		{
-			return false;
+			return 0;
 		}
-		if (x == 2 && y == 1)
-		{
-			return false;
-		}
-		return m.at(layer)[y][x];
-	}
 
-	char getAdjacentBugs(const map& m, const int& layer, const size_t& x, const size_t& y)
-	{
 		char adjacentBugs = 0;
-		if (get(m, x + 1, y)) { adjacentBugs++; }
-		if (get(m, x - 1, y)) { adjacentBugs++; }
-		if (get(m, x, y + 1)) { adjacentBugs++; }
-		if (get(m, x, y - 1)) { adjacentBugs++; }
+		if (y == 0 && x == 0)
+		{
+			if (m.at(layer + 1)[2][1]) { adjacentBugs++; }
+			if (m.at(layer + 1)[1][2]) { adjacentBugs++; }
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 0 && x == 4)
+		{
+			if (m.at(layer + 1)[1][2]) { adjacentBugs++; }
+			if (m.at(layer + 1)[2][3]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 4 && x == 0)
+		{
+			if (m.at(layer + 1)[2][1]) { adjacentBugs++; }
+			if (m.at(layer + 1)[3][2]) { adjacentBugs++; }
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 4 && x == 4)
+		{
+			if (m.at(layer + 1)[2][3]) { adjacentBugs++; }
+			if (m.at(layer + 1)[3][2]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 2 && x == 1)
+		{
+			for (size_t i = 0; i < 5; i++) { if (m.at(layer - 1)[i][0]) { adjacentBugs++; } }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 2 && x == 3)
+		{
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			for (size_t i = 0; i < 5; i++) { if (m.at(layer - 1)[i][4]) { adjacentBugs++; } }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 1 && x == 2)
+		{
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			for (size_t i = 0; i < 5; i++) { if (m.at(layer - 1)[0][i]) { adjacentBugs++; } }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 3 && x == 2)
+		{
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			for (size_t i = 0; i < 5; i++) { if (m.at(layer - 1)[4][i]) { adjacentBugs++; } }
+			return adjacentBugs;
+		}
+
+		if (x == 0)
+		{
+			if (m.at(layer + 1)[2][1]) { adjacentBugs++; }
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (x == 4)
+		{
+			if (m.at(layer + 1)[2][3]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 0)
+		{
+			if (m.at(layer + 1)[1][2]) { adjacentBugs++; }
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (y == 4)
+		{
+			if (m.at(layer + 1)[3][2]) { adjacentBugs++; }
+			if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+			if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+			if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+			return adjacentBugs;
+		}
+
+		if (m.at(layer)[y][x + 1]) { adjacentBugs++; }
+		if (m.at(layer)[y][x - 1]) { adjacentBugs++; }
+		if (m.at(layer)[y + 1][x]) { adjacentBugs++; }
+		if (m.at(layer)[y - 1][x]) { adjacentBugs++; }
+
 		return adjacentBugs;
 	}
-	*/
+
+	multimap progress(const multimap& m)
+	{
+		multimap newm = m;
+		for (size_t l = 1; l < m.size() - 1; l++) // skip first and last
+		{
+			for (size_t y = 0; y < m.at(l).size(); y++)
+			{
+				for (size_t x = 0; x < m.at(l)[y].size(); x++)
+				{
+					if (m.at(l)[y][x])
+					{
+						newm.at(l)[y][x] = getAdjacentBugs(m, l, x, y) == 1;
+					}
+					else
+					{
+						char bugs = getAdjacentBugs(m, l, x, y);
+						newm.at(l)[y][x] = (bugs == 1 || bugs == 2);
+					}
+				}
+			}
+		}
+		return newm;
+	}
+
+	int countBugs(const multimap& m)
+	{
+		int count = 0;
+		for (size_t l = 0; l < m.size(); l++)
+		{
+			for (size_t y = 0; y < m.at(l).size(); y++)
+			{
+				for (size_t x = 0; x < m.at(l)[y].size(); x++)
+				{
+					if (m.at(l)[y][x])
+					{
+						count++;
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	std::string inspect(const multimap& m)
+	{
+		std::stringstream ss;
+		for (size_t i = 0; i < m.size(); i++)
+		{
+			ss << inspect(m.at(i));
+			ss << "\n\n";
+		}
+		return ss.str();
+	}
+
+	TEST(NAME, ExampleB)
+	{
+		std::string input = R"(
+....#
+#..#.
+#..##
+..#..
+#....)";
+
+		multimap m;
+		m.reserve(20);
+		for (int i = 0; i < 20; i++)
+		{
+			if (i == 10)
+			{
+				m.push_back(create(input));
+			}
+			else
+			{
+				m.push_back(map(5, std::vector<bool>(5, false)));
+			}
+		}
+
+		for (size_t i = 0; i < 10; i++)
+		{
+			m = progress(m);
+		}
+
+		EXPECT_EQ(countBugs(m), 99);
+	}
+
+	TEST(NAME, InputB)
+	{
+		std::string input = R"(
+##.#.
+.#.##
+.#...
+#..#.
+.##..)";
+
+		multimap m;
+		m.reserve(1000);
+		for (int i = 0; i < 1000; i++)
+		{
+			if (i == 500)
+			{
+				m.push_back(create(input));
+			}
+			else
+			{
+				m.push_back(map(5, std::vector<bool>(5, false)));
+			}
+		}
+
+		for (size_t i = 0; i < 200; i++)
+		{
+			m = progress(m);
+		}
+
+		EXPECT_EQ(countBugs(m), 1921);
+	}
 }
+
+
