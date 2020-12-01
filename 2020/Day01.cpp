b18b1ca4bc068a57488e6a09aa28aad3e83bdf02
Lukas Pirkl <pirkl.lukas@gmail.com>
2020-12-01 19:59:37 +0100

    2020 Day01

---
+++ 2020/Day01.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+#include "aoc.h"
+
+namespace
+{
+	TEST(Day01, InputA)
+	{
+		auto input = aoc::readInputFile("Day01.txt");
+
+		int result = 0;
+		std::vector<int> nums;
+
+		for (std::string line; std::getline(input, line); ) {
+			nums.push_back(std::stoi(line));
+		}
+
+		for (size_t i = 0; i < nums.size(); i++)
+		{
+			for (size_t j = 0; j < nums.size(); j++)
+			{
+				if (i == j) continue;
+				if (nums[i] + nums[j] == 2020)
+				{
+					EXPECT_EQ(nums[i] * nums[j], 259716);
+					return;
+				}
+			}
+		}
+		EXPECT_FALSE(true);
+	}
+
+	TEST(Day01, InputB)
+	{
+		auto input = aoc::readInputFile("Day01.txt");
+
+		int result = 0;
+		std::vector<int> nums;
+
+		for (std::string line; std::getline(input, line); ) {
+			nums.push_back(std::stoi(line));
+		}
+
+		for (size_t i = 0; i < nums.size(); i++)
+		{
+			for (size_t j = 0; j < nums.size(); j++)
+			{
+				if (i == j) continue;
+				for (size_t k = 0; k < nums.size(); k++)
+				{
+					if (i == k || j == k) continue;
+					if (nums[i] + nums[j] + nums[k] == 2020)
+					{
+						EXPECT_EQ(nums[i] * nums[j] * nums[k], 120637440);
+						return;
+					}
+				}
+			}
+		}
+		EXPECT_FALSE(true);
+	}
+
+
+}
---
+++ 2020/pch.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <queue>
